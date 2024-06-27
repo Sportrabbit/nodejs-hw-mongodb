7f9f81765190756fa3121dddc28c820afe17aa92
Sportrabbit <145197781+Sportrabbit@users.noreply.github.com>
2024-06-27 19:44:47 +0300

    Update auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import CreateError from 'http-errors';
 import { User } from '../validation/userValidation.js';
