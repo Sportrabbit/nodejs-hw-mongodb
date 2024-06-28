43668c880fdc886fcbce06aeb4aa89db2a22620c
Sportrabbit <145197781+Sportrabbit@users.noreply.github.com>
2024-06-28 18:54:05 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import { env } from './utils/env.js';
 import contactsRouter from './routers/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import { authRouter } from './routers/auth.js';
+import  authRouter  from './routers/auth.js';
 
 const PORT = Number(env('PORT', 3000));
 
