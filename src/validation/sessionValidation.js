6f8c4ed758695ea80a343f2ee49457c07b2afe9e
Sportrabbit <145197781+Sportrabbit@users.noreply.github.com>
2024-06-28 19:02:25 +0300

    Update sessionValidation.js

--- src/validation/sessionValidation.js
+++ src/validation/sessionValidation.js
@@ -1,10 +1,8 @@
 import Joi from 'joi';
 
-const { date } = Joi;
-
 export const Session = Joi.object({
     userId: Joi.string().required(),
     sessionId: Joi.string().required(),
-    createdAt: date().default(Date.now),
-    updatedAt: date().default(Date.now),
+    createdAt: Joi.date().default(() => new Date(), 'time of creation'),
+    updatedAt: Joi.date().default(() => new Date(), 'time of update'),
 });
