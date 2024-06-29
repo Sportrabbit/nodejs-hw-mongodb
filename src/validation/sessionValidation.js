54bb4ec89314deb668ad14cfe338db60714c0d5b
Sportrabbit <145197781+Sportrabbit@users.noreply.github.com>
2024-06-29 14:32:22 +0300

    up

--- src/validation/sessionValidation.js
+++ src/validation/sessionValidation.js
@@ -3,6 +3,4 @@ import Joi from 'joi';
 export const Session = Joi.object({
     userId: Joi.string().required(),
     sessionId: Joi.string().required(),
-    createdAt: Joi.date().default(() => new Date(), 'time of creation'),
-    updatedAt: Joi.date().default(() => new Date(), 'time of update'),
 });
--- src/validation/userValidation.js
+++ src/validation/userValidation.js
@@ -4,6 +4,4 @@ export const User = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
-    createdAt: Joi.date().default(() => new Date(), 'time of creation'),
-    updatedAt: Joi.date().default(() => new Date(), 'time of update'),
 });
