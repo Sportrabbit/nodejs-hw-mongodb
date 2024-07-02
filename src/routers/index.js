a705a825c5f6c1ed968943c6e610a1516ebd9667
Sportrabbit <145197781+Sportrabbit@users.noreply.github.com>
2024-07-02 19:52:49 +0300

    Update index.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -5,7 +5,7 @@ import router from "./contacts";
 const rootRouter = Router();
 
 rootRouter.use('/auth', authRouter);
-rootRouter.use('/students', router);
+rootRouter.use('/contacts', router);
 rootRouter.use('/test', testRouter);
 
 export default rootRouter;
